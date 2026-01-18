4c74082bced9ef1cd05ef3a17e9b840ad2ef4040
DiogoFilhoDeMorais <analise.federal@gmail.com>
2026-01-18 14:07:28 +0000

    Creating Repository

---
+++ src/main/java/com/natixis/payment/repository/PaymentRepository.java
@@ -0,0 +1,9 @@
+package com.natixis.payment.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.natixis.payment.entity.Payment;
+
+public interface PaymentRepository extends JpaRepository<Payment, Long> {
+    
+}
